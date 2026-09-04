cf112e89b49f10f098061606d5f63f3939661343
andrea-azze <andreaazzerboni2000@gmail.com>
2026-09-05 01:08:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Tiene i file dell'app nella cache del telefono, così parte anche senza rete.
    I dati delle liste NON passano da qui: restano in localStorage. */
 
-const CACHE = "azze-list-v14";
+const CACHE = "azze-list-v15";
 const FILES = [
   "./",
   "./index.html",
